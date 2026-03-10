0a1c2b0cb4df283cb6e366968b57876a60644da5
hiratsuka <keipon1013hmas@icloud.com>
2026-03-10 11:26:52 +0900

    0310-09

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -279,7 +279,6 @@ const Cart = () => {
                             </div>
                           </div>
                         </div>
-                        </div>
                       </div>
                     </div>
                   );
